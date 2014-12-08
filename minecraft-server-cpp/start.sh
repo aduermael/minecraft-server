7784074046e4e97b74e0f94c6aaec0d8576774f0
Adrien Duermael <ad@pixowl.com>
2014-12-07 19:36:36 -0800

    Update web admin credentials when starting minecraft-server-cpp container

--- minecraft-server-cpp/start.sh
+++ minecraft-server-cpp/start.sh
@@ -3,7 +3,7 @@
 if [ -d "/minecraft-data/$1" ]
 	then 
 		cd /minecraft-data/${1}
-		ls | grep -v 'world' | grep -v 'whitelist.sqlite' | grep -v 'banlist.sqlite' | grep -v 'favicon.png' | grep -v 'webadmin.ini' | grep -v 'settings.ini' | grep -v 'logs' | xargs rm -r
+		ls | grep -v 'world' | grep -v 'whitelist.sqlite' | grep -v 'banlist.sqlite' | grep -v 'favicon.png' | grep -v 'settings.ini' | grep -v 'logs' | xargs rm -r
 	else
 		mkdir /minecraft-data/${1}
 		cd /minecraft-data/${1}
@@ -12,13 +12,16 @@ if [ -d "/minecraft-data/$1" ]
 cp -r /MCServer/* /minecraft-data/${1}/
 rm -r /MCServer
 
-if [ -f "/minecraft-data/$1/webadmin.ini" ]
-	then
-		echo "webadmin.ini is already there."
-	else
-		echo "create webadmin.ini"
-		mv /srv/webadmin.ini /minecraft-data/${1}
-	fi
-	
+
+echo "create webadmin.ini"
+mv /srv/webadmin.ini /minecraft-data/${1}/
+
+sed "s,USER_FIELD,$2," < /minecraft-data/${1}/webadmin.ini > /minecraft-data/${1}/webadmin.ini.tmp1
+sed "s,PASS_FIELD,$3," < /minecraft-data/${1}/webadmin.ini.tmp1 > /minecraft-data/${1}/webadmin.ini.tmp2
+
+rm /minecraft-data/${1}/webadmin.ini
+rm /minecraft-data/${1}/webadmin.ini.tmp1
+mv /minecraft-data/${1}/webadmin.ini.tmp2 /minecraft-data/${1}/webadmin.ini
+
 
 /bin/bash
