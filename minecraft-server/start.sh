20c3a74349e25b388ff72180f24f39990df032bf
Adrien Duermael <adrien@duermael.com>
2023-08-04 23:46:53 +0200

    fix minecraft-server Dockerfile

--- minecraft-server/start.sh
+++ minecraft-server/start.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-# Default version: 1.9
+# Default version: 1.20.1
 # Can be overriten passing 2 parameters
 # 1: memory limit, 2: server version
-version="1.9"
+version="1.20.1"
 if [ -n "$2" ]
 then
 	version="$2"
 fi
 
-if [ ! -f "/data/minecraft_server.${version}.jar" ]
-then
-    curl "https://s3.amazonaws.com/Minecraft.Download/versions/${version}/minecraft_server.${version}.jar" -o "/data/minecraft_server.${version}.jar"
-fi
+curl https://launchermeta.mojang.com/mc/game/version_manifest.json > version_manifest.json
+
+cat version_manifest.json | jq -r ".versions | .[] | select(.id==\"$version\") | .url" > server_manifest_url
+
+curl $(cat server_manifest_url) > server_manifest.json
+
+cat server_manifest.json | jq -r .downloads.server.url > server_url
+
+curl $(cat server_url) -o "/data/minecraft_server.${version}.jar"
 
 if [ ! -f /data/eula.txt ]
 then
